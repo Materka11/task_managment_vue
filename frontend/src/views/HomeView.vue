41193fd099fba442d6bcc544ec57b2e559e227b1
Materka11 <arekmaterka11@gmail.com>
2024-09-17 09:43:11 +0200

    Fix: Hover buttons

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -16,12 +16,12 @@
         <div class="flex gap-10 lg:gap-20">
           <RouterLink
             to="/login"
-            class="px-5 py-2 rounded-lg bg-blue-500 text-white text-center font-bold sm:w-32 sm:px-6 sm:py-3 sm:text-xl lg:w-48 lg:px-8 lg:py-4 lg:text-2xl"
+            class="px-5 py-2 rounded-lg bg-blue-500 text-white text-center font-bold sm:w-32 sm:px-6 sm:py-3 sm:text-xl lg:w-48 lg:px-8 lg:py-4 lg:text-2xl hover:bg-white hover:text-blue-500 hover:border-blue-500 hover:border-2"
             >Log In</RouterLink
           >
           <RouterLink
             to="/signup"
-            class="px-5 py-2 rounded-lg bg-blue-500 text-white text-center font-bold sm:w-32 sm:px-6 sm:py-3 sm:text-xl lg:w-48 lg:px-8 lg:py-4 lg:text-2xl"
+            class="px-5 py-2 rounded-lg bg-white text-blue-500 border-blue-500 border-2 text-center font-bold sm:w-32 sm:px-6 sm:py-3 sm:text-xl lg:w-48 lg:px-8 lg:py-4 lg:text-2xl hover:bg-blue-500 hover:text-white"
             >Sign Up</RouterLink
           >
         </div>
