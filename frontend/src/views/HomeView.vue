26af2e6457f643e93e450b1ad235dd9b0d1eda3b
Materka11 <arekmaterka11@gmail.com>
2024-09-17 11:17:34 +0200

    Feature: Exchange static links to reusable

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SubmitButton from "@/components/SubmitButton.vue"
+</script>
 
 <template>
   <main class="flex flex-col h-screen">
@@ -14,16 +16,8 @@
           </h3>
         </section>
         <div class="flex gap-10 lg:gap-20">
-          <RouterLink
-            to="/login"
-            class="px-5 py-2 rounded-lg bg-blue-500 text-white text-center font-bold sm:w-32 sm:px-6 sm:py-3 sm:text-xl lg:w-48 lg:px-8 lg:py-4 lg:text-2xl hover:bg-white hover:text-blue-500 hover:border-blue-500 hover:border-2"
-            >Log In</RouterLink
-          >
-          <RouterLink
-            to="/signup"
-            class="px-5 py-2 rounded-lg bg-white text-blue-500 border-blue-500 border-2 text-center font-bold sm:w-32 sm:px-6 sm:py-3 sm:text-xl lg:w-48 lg:px-8 lg:py-4 lg:text-2xl hover:bg-blue-500 hover:text-white"
-            >Sign Up</RouterLink
-          >
+          <SubmitButton :is-link="true" label="Log In" link="/login" />
+          <SubmitButton :is-link="true" label="Sign Up" link="/signup" :is-primary="false" />
         </div>
       </div>
     </div>
