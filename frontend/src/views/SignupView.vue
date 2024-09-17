d2731f2df3d51585f98be504163d708239ca0edc
Materka11 <arekmaterka11@gmail.com>
2024-09-17 12:40:27 +0200

    Feature: Signup view

---
+++ frontend/src/views/SignupView.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import InputForm from "@/components/InputForm.vue"
+import FormTemplate from "@/templates/FormTemplate.vue"
+
+const INFO_TEXT: [string, string, string] = ["Do you have account?", "/login", "Login Now"]
+</script>
+
+<template>
+  <FormTemplate
+    title="Welcome"
+    subtitle="Sign Up with Email"
+    button-text="Sign Up"
+    :info-text="INFO_TEXT"
+    :is-img="false"
+  >
+    <InputForm id="name" label="Name" :required="true" type="text" />
+    <InputForm id="surname" label="Surname" :required="true" type="text" />
+    <InputForm id="email" label="Email" :required="true" type="email" />
+    <InputForm id="occupation" label="Occupation" :required="true" type="text" />
+    <InputForm id="password" label="Password" :required="true" type="password" minlength="8" />
+    <InputForm
+      id="confirm-password"
+      label="Confirm Password"
+      :required="true"
+      type="password"
+      minlength="8"
+    />
+  </FormTemplate>
+</template>
