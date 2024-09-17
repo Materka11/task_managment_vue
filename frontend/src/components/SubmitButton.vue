2500d160658f7bfa3d4eece9d6698e071c274994
Materka11 <arekmaterka11@gmail.com>
2024-09-17 11:16:22 +0200

    Feature: Submit form

---
+++ frontend/src/components/SubmitButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { RouteLocationRaw } from "vue-router"
+
+const {
+  isLink = false,
+  isPrimary = true,
+  label,
+  link
+} = defineProps<{
+  isLink?: boolean
+  isPrimary?: boolean
+  label: string
+  link?: string | RouteLocationRaw
+}>()
+
+if (isLink && !link) {
+  throw new Error("Prop 'link' is required when 'isLink' is true.")
+}
+
+const styles = isPrimary
+  ? "flex justify-center items-center px-5 w-full rounded-lg bg-blue-500 text-white text-center font-bold h-16 text-nowrap sm:w-32 sm:px-6 sm:text-xl lg:w-48 lg:px-8 lg:text-2xl hover:bg-white hover:text-blue-500 hover:border-blue-500 hover:border-2"
+  : "flex justify-center items-center px-5 w-full rounded-lg bg-white text-blue-500 border-blue-500 border-2 text-center font-bold h-16 text-nowrap sm:w-32 sm:px-6 sm:text-xl lg:w-48 lg:px-8 lg:text-2xl hover:bg-blue-500 hover:text-white"
+</script>
+
+<template>
+  <RouterLink v-if="isLink && link" :to="link" :class="styles">{{ label }}</RouterLink>
+  <button v-else :class="styles">{{ label }}</button>
+</template>
