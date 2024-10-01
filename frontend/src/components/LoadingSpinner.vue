8e13ed31a9e8fa7cb0189509678f46d015eb5d73
Materka11 <arekmaterka11@gmail.com>
2024-10-01 09:47:22 +0200

    Feature: Loading spinner component

---
+++ frontend/src/components/LoadingSpinner.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { loading } = defineProps<{
+  loading: boolean
+}>()
+</script>
+
+<template>
+  <div
+    v-if="loading"
+    class="w-12 h-12 border-4 border-t-4 border-t-blue-500 border-gray-200 rounded-full animate-spin"
+  ></div>
+</template>
