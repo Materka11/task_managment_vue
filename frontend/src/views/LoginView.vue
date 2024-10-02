425063d2349e535297d4656252d5472309608bed
Materka11 <arekmaterka11@gmail.com>
2024-10-02 10:54:17 +0200

    Fix: Login View

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -18,15 +18,22 @@ onMounted(() => {
 <template>
   <FormTemplate title="Welcome" subtitle="Login with Email">
     <form class="flex flex-col gap-6">
-      <!-- <InputForm id="email" label="Email" :required="true" type="email" /> -->
-      <!-- <InputForm id="password" label="Password" :is-password="true" ><input
-      :id="id"
-      class="border-2 border-blue-400 rounded-lg h-12 px-4 outline-none leading-10"
-      type="password"
-      required
-      minlength="8"
-      v-model="value"
-    /></InputForm> -->
+      <InputForm id="email" label="Email">
+        <input
+          id="email"
+          class="border-2 border-blue-400 rounded-lg h-12 px-4 outline-none leading-10"
+          type="email"
+          required
+        />
+      </InputForm>
+      <InputForm id="password" label="Password" :is-password="true"
+        ><input
+          id="password"
+          class="border-2 border-blue-400 rounded-lg h-12 px-4 outline-none leading-10"
+          type="password"
+          required
+          minlength="8"
+      /></InputForm>
       <div class="flex flex-col justify-center items-center gap-4">
         <SubmitButton label="Log In" />
         <span
