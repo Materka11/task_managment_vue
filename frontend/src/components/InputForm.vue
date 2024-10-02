20ec07c171de846541c726cbcee84973e4ad70b9
Materka11 <arekmaterka11@gmail.com>
2024-10-02 10:54:39 +0200

    Fix: Deleted unnecessary spaces

--- frontend/src/components/InputForm.vue
+++ frontend/src/components/InputForm.vue
@@ -13,13 +13,11 @@ const {
 <template>
   <div v-if="isPassword" class="flex flex-col gap-2">
     <label :for="id" class="text-blue-400 px-4 font-bold">{{ label }}</label>
-
     <slot></slot>
     <button class="self-end text-gray-500">Forgot your password?</button>
   </div>
   <div v-else class="flex flex-col gap-2">
     <label :for="id" class="text-blue-400 px-4 font-bold">{{ label }}</label>
-
     <slot></slot>
   </div>
 </template>
