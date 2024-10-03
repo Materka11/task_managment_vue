fc44a9607f514bea1a67ee273648037c224302d7
Materka11 <arekmaterka11@gmail.com>
2024-10-03 16:07:50 +0200

    Feature: Login method frontend

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -4,26 +4,70 @@ import FormTemplate from "@/templates/FormTemplate.vue"
 import SubmitButton from "@/components/SubmitButton.vue"
 import { useRoute } from "vue-router"
 import { toast } from "vue3-toastify"
-import { onMounted } from "vue"
+import { onMounted, ref } from "vue"
+import gql from "graphql-tag"
+import { useLazyQuery } from "@vue/apollo-composable"
+
+const LOGIN_QUERY = gql`
+  query Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      user {
+        name
+        createdAt
+        id
+        occupation
+        surname
+        updatedAt
+        email
+      }
+      refreshToken
+      token
+    }
+  }
+`
+const formData = ref({
+  email: "",
+  password: ""
+})
 
 const route = useRoute()
+
+const { load: login, loading } = useLazyQuery(LOGIN_QUERY, {
+  email: formData.value.email,
+  password: formData.value.password
+})
 
 onMounted(() => {
   if (route.query.register === "success") {
     toast.success("Register success")
   }
 })
+
+const handleSubmit = async () => {
+  try {
+    const { login: data } = await login(null, {
+      email: formData.value.email,
+      password: formData.value.password
+    })
+
+    console.log(data)
+  } catch (e) {
+    console.error(e)
+    toast.error("Login failed")
+  }
+}
 </script>
 
 <template>
   <FormTemplate title="Welcome" subtitle="Login with Email">
-    <form class="flex flex-col gap-6">
+    <form class="flex flex-col gap-6" @submit.prevent="handleSubmit">
       <InputForm id="email" label="Email">
         <input
           id="email"
           class="border-2 border-blue-400 rounded-lg h-12 px-4 outline-none leading-10"
           type="email"
           required
+          v-model="formData.email"
         />
       </InputForm>
       <InputForm id="password" label="Password" :is-password="true"
@@ -33,6 +77,7 @@ onMounted(() => {
           type="password"
           required
           minlength="8"
+          v-model="formData.password"
       /></InputForm>
       <div class="flex flex-col justify-center items-center gap-4">
         <SubmitButton label="Log In" />
