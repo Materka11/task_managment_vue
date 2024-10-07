192bf89bb9882a850b573524a56224dee62e3870
Materka11 <arekmaterka11@gmail.com>
2024-10-07 15:00:08 +0200

    Feature: Added dashboard view

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -18,6 +18,11 @@ const router = createRouter({
       path: "/signup",
       name: "signup",
       component: () => import("../views/SignupView.vue")
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: () => import("../views/DashboardView.vue")
     }
   ]
 })
