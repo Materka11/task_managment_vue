d21dd92d6f5a38ee5cb891e01310a3a7cc59656b
Materka11 <arekmaterka11@gmail.com>
2024-10-14 09:03:45 +0200

    Feature: Get me

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,12 +1,40 @@
 <script setup lang="ts">
 import NavDashboard from "@/components/NavDashboard.vue"
-import { onMounted } from "vue"
+import { useQuery } from "@vue/apollo-composable"
+import gql from "graphql-tag"
+import { inject, onMounted } from "vue"
+import type { VueCookies } from "vue-cookies"
 import { useRoute } from "vue-router"
 import { toast } from "vue3-toastify"
 
+const ME_QUERY = gql`
+  query Me {
+    me {
+      name
+      id
+      email
+    }
+  }
+`
+
 const route = useRoute()
 
-onMounted(() => {
+const $cookies = inject<VueCookies>("$cookies")
+
+const { result: meData } = useQuery(
+  ME_QUERY,
+  {},
+  {
+    context: {
+      headers: {
+        Authorization: $cookies?.get("Authorization")
+      }
+    }
+  }
+)
+console.log(meData.value)
+
+onMounted(async () => {
   if (route.query.login === "success") {
     toast.success("Login success")
   }
