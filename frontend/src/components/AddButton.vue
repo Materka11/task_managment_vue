3e3c4c3217b57f0659fad650414a09b0a74b81a5
Materka11 <arekmaterka11@gmail.com>
2024-10-15 11:46:59 +0200

    Feature: AddButton

---
+++ frontend/src/components/AddButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import PlusCircleFillSVG from "@/assets/icons/PlusCircleFillSVG.vue"
+
+const { label, handleClick } = defineProps<{ label: string; handleClick: () => void }>()
+</script>
+
+<template>
+  <button
+    @click="handleClick"
+    class="bg-white text-blue-500 py-2 px-4 rounded-md fill-blue-500 hover:bg-blue-500 hover:text-white hover:fill-white flex items-center space-x-2 border-blue-500 border-2"
+  >
+    <PlusCircleFillSVG />
+    <span class="font-bold">{{ label }}</span>
+  </button>
+</template>
