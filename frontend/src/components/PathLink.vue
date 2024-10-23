11f4c4f4bd1fa863fcb1d7853bdd61e7282cfe59
Materka11 <arekmaterka11@gmail.com>
2024-10-23 13:06:38 +0200

    Feature: PathLink

---
+++ frontend/src/components/PathLink.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { label, href } = defineProps<{ label: string; href: string }>()
+</script>
+
+<template>
+  <a
+    :href="href"
+    class="flex gap-2 w-fit text-gray-500 fill-gray-500 hover:text-blue-500 hover:fill-blue-500"
+    ><slot></slot><span class="font-bold">{{ label }}</span></a
+  >
+</template>
