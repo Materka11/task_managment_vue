21a6d298bf3cb3eaa43b0fbdb13396053403f7b1
Materka11 <arekmaterka11@gmail.com>
2024-10-24 12:04:51 +0200

    Feature: Added new section to mobile menu

--- frontend/src/components/NavDashboard.vue
+++ frontend/src/components/NavDashboard.vue
@@ -7,6 +7,10 @@ import BellSVG from "@/assets/icons/BellSVG.vue"
 import ShortProfile from "./ShortProfile.vue"
 import { ref } from "vue"
 import HamburgerButton from "./HamburgerButton.vue"
+import PathLink from "./PathLink.vue"
+import HomeSVG from "@/assets/icons/HomeSVG.vue"
+import MessageSVG from "@/assets/icons/MessageSVG.vue"
+import NotepadSVG from "@/assets/icons/NotepadSVG.vue"
 
 const { user } = defineProps<{ user: IUser | null }>()
 const isMenuOpen = ref(false)
@@ -48,10 +52,22 @@ const toggleMenu = () => {
         />
         <HamburgerButton :is-menu-open="isMenuOpen" :toggleMenu="toggleMenu" />
       </div>
-      <AddButton label="Create Task" :handle-click="() => {}" />
-      <ActionButton :handle-click="() => {}"
-        ><BellSVG /> <span class="font-bold">Notification</span></ActionButton
-      >
+      <div class="flex flex-col gap-10 w-full py-10">
+        <section class="flex flex-col gap-2 w-full">
+          <AddButton label="Create Task" :handle-click="() => {}" />
+          <ActionButton :handle-click="() => {}"
+            ><BellSVG /> <span class="font-bold">Notification</span></ActionButton
+          >
+        </section>
+        <section class="flex flex-col gap-4 w-full">
+          <PathLink label="Home" href="/home"><HomeSVG /></PathLink>
+          <PathLink label="Message" href="/message"
+            ><MessageSVG class="stroke-gray-500 group-hover:stroke-blue-500"
+          /></PathLink>
+          <PathLink label="Activities" href="/activities"><NotepadSVG /></PathLink>
+        </section>
+        <hr class="border-2" />
+      </div>
     </div>
   </nav>
 </template>
