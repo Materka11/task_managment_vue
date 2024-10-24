e31896c4d034d23ef3e54041208ff34324c94cac
Materka11 <arekmaterka11@gmail.com>
2024-10-24 12:05:15 +0200

    Fix: Styles for PathLink

--- frontend/src/components/PathLink.vue
+++ frontend/src/components/PathLink.vue
@@ -5,7 +5,7 @@ const { label, href } = defineProps<{ label: string; href: string }>()
 <template>
   <a
     :href="href"
-    class="flex gap-2 w-fit text-gray-500 fill-gray-500 hover:text-blue-500 hover:fill-blue-500"
+    class="flex gap-2 w-fit text-gray-500 fill-gray-500 hover:text-blue-500 hover:fill-blue-500 group"
     ><slot></slot><span class="font-bold">{{ label }}</span></a
   >
 </template>
