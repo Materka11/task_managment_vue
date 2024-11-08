c46f288b98005b5dfef487d6d832e3dd6d186004
Materka11 <arekmaterka11@gmail.com>
2024-11-08 16:54:39 +0100

    Feature: Added Help icon

--- frontend/src/components/NavDashboard.vue
+++ frontend/src/components/NavDashboard.vue
@@ -17,6 +17,7 @@ import GroupSVG from "@/assets/icons/GroupSVG.vue"
 import GoalsSVG from "@/assets/icons/GoalsSVG.vue"
 import ProjectSVG from "@/assets/icons/ProjectSVG.vue"
 import SettingsSVG from "@/assets/icons/SettingsSVG.vue"
+import HelpSVG from "@/assets/icons/HelpSVG.vue"
 
 const { user } = defineProps<{ user: IUser | null }>()
 const isMenuOpen = ref(false)
@@ -88,8 +89,8 @@ const toggleMenu = () => {
         <hr class="border-2" />
         <section class="flex flex-col gap-4 w-full">
           <ButtonOrLink label="Settings" href="/settings" type="a"><SettingsSVG /></ButtonOrLink>
+          <ButtonOrLink label="Help" href="/help" type="a"><HelpSVG /></ButtonOrLink>
         </section>
-        <hr class="border-2" />
       </div>
     </div>
   </nav>
